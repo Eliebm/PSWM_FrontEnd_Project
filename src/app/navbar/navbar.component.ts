82f840bc8960c26610452e658c84501f84ef4d00
Eliebm <eliebm17@live.com>
2023-01-23 17:44:56 +0200

    navbar with bootstrap

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'pm-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss'],
+
 })
 export class NavbarComponent implements OnInit {
 
